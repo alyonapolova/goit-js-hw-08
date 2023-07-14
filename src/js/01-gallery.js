46fbc2c19f2f2aecddc4172155416bed6ef974a8
Alyona <polova_alyona0907@ukr.net>
2023-07-14 20:04:10 +0200

    01, 02 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,40 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryOfImg = document.querySelector('.gallery');
+
+const imgGalery = galleryItems.map(image => {
+  const newImg = `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`;
+  return newImg;
+});
+
+galleryOfImg.insertAdjacentHTML('afterbegin', imgGalery.join(''));
+
+galleryOfImg.addEventListener('click', openOriginalImg);
+
+function openOriginalImg(event) {
+  event.preventDefault();
+
+  if (event.target.tagName !== 'IMG') {
+    return;
+  }
+}
+createLightbox();
+
+function createLightbox() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  console.log(data.seconds);
+  const currentTime = data.seconds;
+  //console.log(typeof currentTime);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+  // data is an object containing properties specific to that event
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const newTime = localStorage.getItem('videoplayer-current-time');
+const savedTime = JSON.parse(newTime);
+
+console.log(typeof savedTime);
+
+if (savedTime !== null) {
+  player
+    .setCurrentTime(savedTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
