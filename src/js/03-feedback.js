05bbb45e20ea09a74520cf811dfebdb4e1744b8f
Alyona <polova_alyona0907@ukr.net>
2023-07-15 16:11:35 +0200

    03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,105 @@
-// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы.
-//Пусть ключом для хранилища будет строка "feedback-form-state".
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные,
-//заполняй ими поля формы.В противном случае поля должны быть пустыми.
-// При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email,
-//message и текущими их значениями в консоль.
-// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
-console.log(formEl);
-// formEl.addEventListener('submit', formFilling);
+const form = document.querySelector('.feedback-form');
 
-// function formFilling(event) {
-//   event.preventDefault();
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+let newData = {};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Всі поля мають бути заповнені!');
+  }
+
+  const newObj = {
+    email: email.value,
+    messasge: message.value,
+  };
+  console.log(newObj);
+
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(e) {
+  //     console.log(e.target.name);
+  //   console.log(e.target.value);
+  newData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(newData));
+}
+
+function populateTextarea() {
+  const dataFromLocalStorage = localStorage.getItem('feedback-form-state');
+  const savedData = JSON.parse(dataFromLocalStorage);
+
+  //console.log(Object.keys(savedData));
+  //console.log(Object.values(savedData));
+  if (savedData) {
+    Object.entries(savedData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+  //   if (savedData) {
+  //     for (const key in savedData) {
+  //       form[key].value = savedData[key];
+  //     }
+  //   }
+}
+
+populateTextarea();
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   email: document.querySelector('.feedback-form input'),
+//   textarea: document.querySelector('.feedback-form textarea'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.email.addEventListener('input', throttle(onEmailInput, 500));
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+// let newData = {};
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+
+//   e.currentTarget.reset();
+//   localStorage.removeItem('feedback-form-state');
+//   console.log(newData);
 // }
+
+// function onEmailInput(e) {
+//   const emailValue = e.currentTarget.value;
+//   newData.email = emailValue;
+//   saveDataToLocalStorage();
+// }
+
+// function onTextareaInput(e) {
+//   const messageValue = e.currentTarget.value;
+//   newData.message = messageValue;
+//   saveDataToLocalStorage();
+// }
+
+// function saveDataToLocalStorage() {
+//   localStorage.setItem('feedback-form-state', JSON.stringify(newData));
+// }
+
+// function populateTextarea() {
+//   const dataFromLocalStorage = localStorage.getItem('feedback-form-state');
+//   const savedData = JSON.parse(dataFromLocalStorage);
+
+//   if (savedData) {
+//     newData = savedData;
+//     refs.email.value = savedData.email;
+//     refs.textarea.value = savedData.message;
+//   }
+// }
+
+// populateTextarea();
